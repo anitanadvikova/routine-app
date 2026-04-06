48828bc06133c662d0916324a5ea4978a0139b32
anitanadvikova <anitanadvikova@yandex.ru>
2026-04-06 13:31:58 +0300

    Initial Commit

---
+++ RoutineAppTests/RoutineAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  RoutineAppTests.swift
+//  RoutineAppTests
+//
+//  Created by Анита Надвикова on 06.04.2026.
+//
+
+import Testing
+@testable import RoutineApp
+
+struct RoutineAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
